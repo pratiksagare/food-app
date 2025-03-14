114e8219c1b72c946581fb13466af9f52e35bb22
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2025-03-14 22:21:11 +0530

    psagare : save cart data and retrive on signin

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,18 +1,26 @@
 "use client"
+import { setCart } from "@/lib/store/slices/cartSlice";
 // import { signIn } from '@/app/api/auth';
 
 import { signIn } from "next-auth/react";
+import { useDispatch, useSelector } from "react-redux";
 
 const page = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart);
   return (
     <form
-      action={async () => {
-        await signIn("github")
+      onSubmit={async (e) => {
+        e.preventDefault();
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+        await signIn("github");
+        dispatch(setCart(localStorage.getItem("cart")));
+        localStorage.clear();
       }}
     >
       <button type="submit">Signin with GitHub</button>
     </form>
   )
-} 
+}
 
 export default page
